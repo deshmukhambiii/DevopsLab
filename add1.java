6210f867a83ad2e10dc251984af01fbb279c79e5
Shreya <shreyabhosale1509@gmail.com>
2026-02-12 10:51:02 +0530

    added tests#2

--- add1.java
+++
@@ -1,9 +0,0 @@
-public class add1 {
-    public int addnumbers(int x, int y)
-    {
-        int sum = 0;
-        sum = x + y;
-        System.out.println("sum is :" + sum);
-        return sum;
-    }
-}
